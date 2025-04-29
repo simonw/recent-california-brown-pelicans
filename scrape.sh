ae42d970abb1d39f48ed2b0d907b9a00552a7638
Simon Willison <swillison@gmail.com>
2025-04-28 19:02:58 -0700

    trying to exclude dead pelicans

--- scrape.sh
+++ scrape.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./download.sh 'https://api.inaturalist.org/v1/observations?taxon_id=123829&per_page=30&order=desc&order_by=observed_on'
+./download.sh 'https://api.inaturalist.org/v1/observations?taxon_id=123829&per_page=30&order=desc&order_by=observed_on&term_id=17&without_term_value_id=19'
